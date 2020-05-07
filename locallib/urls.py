4d73c57b720fa81181254df62c3f1749cba1a300
Chege Bryan <cbbryan8@gmail.com>
2020-05-07 16:23:41 +0300

    [Chore 🔗] Add static files URL mapping

--- locallib/urls.py
+++ locallib/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +28,5 @@ urlpatterns = [
 urlpatterns += [
     path('', RedirectView.as_view(url='catalog/', permanent=True))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
