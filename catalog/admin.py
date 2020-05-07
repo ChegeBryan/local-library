aa30992e73403ad51c2c1f1aaff7155a7f96a15d
Chege Bryan <cbbryan8@gmail.com>
2020-05-07 23:52:02 +0300

    🎨 Add Book form on Admin-Author detail view

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,10 +3,17 @@ from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
 
 
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name',
                     'date_of_birth', 'date_of_death',)
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death'), ]
+
+    inlines = [BookInline, ]
 
 
 class BookInstanceInline(admin.TabularInline):
