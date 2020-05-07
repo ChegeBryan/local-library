2fd074c7f1b6f46c8a91f44d89bcbc8830b7283b
Chege Bryan <cbbryan8@gmail.com>
2020-05-07 21:36:00 +0300

    🔨 Introduce Author Admin page customisation

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 
 from .models import Author, Genre, Book, BookInstance, Language
 
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Book)
-admin.site.register(Author)
+# admin.site.register(Author)
 admin.site.register(BookInstance)
 admin.site.register(Genre)
 admin.site.register(Language)
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
