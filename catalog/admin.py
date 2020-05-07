9851ce22d84873e948a2bc5717f6bacea157a4cf
Chege Bryan <cbbryan8@gmail.com>
2020-05-07 22:47:34 +0300

    🎨 Add Genre field on Admin-Books displayed

--- catalog/admin.py
+++ catalog/admin.py
@@ -10,7 +10,7 @@ class AuthorAdmin(admin.ModelAdmin):
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author')
+    list_display = ('title', 'author', 'display_genre',)
 
 
 @admin.register(BookInstance)
