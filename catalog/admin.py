2dcccd190d331cb018891683383a1c0caeba4df2
Chege Bryan <cbbryan8@gmail.com>
2020-05-07 21:40:13 +0300

    🔨 Introduce Admin-BookInstance page customisation

--- catalog/admin.py
+++ catalog/admin.py
@@ -12,9 +12,14 @@ class BookAdmin(admin.ModelAdmin):
     pass
 
 
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+
 # admin.site.register(Book)
 # admin.site.register(Author)
-admin.site.register(BookInstance)
+# admin.site.register(BookInstance)
 admin.site.register(Genre)
 admin.site.register(Language)
 # Register the admin class with the associated model
