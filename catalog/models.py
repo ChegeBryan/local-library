935c2cb65b3af5a4b293bc3ea7834359bcfd724b
Chege Bryan <cbbryan8@gmail.com>
2020-05-07 19:33:42 +0300

    📂 Define Language DB Model
    📂 Define Language DB Model

--- catalog/models.py
+++ catalog/models.py
@@ -31,6 +31,9 @@ class Book(models.Model):
     genre = models.ManyToManyField(
         Genre, help_text='Select a genre for this book')
 
+    language = models.ForeignKey(
+        'Language', on_delete=models.SET_NULL, null=True)
+
     def __str__(self):
         """String for representing the Model object."""
         return self.title
@@ -38,6 +41,18 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+
+
+class Language(models.Model):
+    """ Model respresenting the langauge """
+    name = models.CharField(max_length=200,
+                            help_text="Enter the books natural language(E.g \
+                                        English, French, Japanesse)"
+                            )
+
+    def __str__(self):
+        """ String representing the MOdel object (in Admin site etc.)"""
+        return self.name
 
 
 class BookInstance(models.Model):
