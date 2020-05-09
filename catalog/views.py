0ea712a08d83bc1c617e8c9b25defd79617f6e57
Chege Bryan <cbbryan8@gmail.com>
2020-05-09 21:00:53 +0300

    🎨 Count books containing 'ger' in title

--- catalog/views.py
+++ catalog/views.py
@@ -17,11 +17,15 @@ def index(request):
     # The 'all()' is implied by default
     num_authors = Author.objects.count()
 
+    # Books (title contains 'Ger') ignore case
+    num_ger_books = Book.objects.filter(title__icontains='ger').count()
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_ger_books': num_ger_books,
     }
 
     # Render the HTML temlate ,index.html with the data in context
