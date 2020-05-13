dc0cbe01e43866936e49fc8804556e8955c5951a
Chege Bryan <cbbryan8@gmail.com>
2020-05-13 14:08:05 +0300

    ⚙ Update session data with every request

--- locallib/settings.py
+++ locallib/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Update database/send cookies on every request
+SESSION_SAVE_EVERY_REQUEST = True
