fbd926062ea5359b800286b625d196ed3a829b7a
Chege Bryan <cbbryan8@gmail.com>
2020-05-15 08:45:49 +0300

    ❕ Validate renewal date is within 4 wks range

--- catalog/forms.py
+++ catalog/forms.py
@@ -15,3 +15,8 @@ class RenewBookForm(forms.Form):
         # CHeck if a date is not in the past
         if data < datetime.date.today():
             raise ValidationError(_('Invalid date - renewal in the past'))
+
+        # Check if a date is in the allowed range (+4 weeks from today)
+        if data > datetime.date.today() + datetime.timedelta(weeks=4):
+            raise ValidationError(
+                _('Invalid date - renewal more than 4 weeks ahead'))
