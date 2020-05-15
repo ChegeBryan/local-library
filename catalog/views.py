c025d746fc7f51d12e4fefaf49a198baa1578689
Chege Bryan <cbbryan8@gmail.com>
2020-05-15 10:31:36 +0300

    🗂  Add update author form view

--- catalog/views.py
+++ catalog/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 from catalog.models import Book, Author, BookInstance, Genre
 from catalog.forms import RenewBookForm
@@ -137,3 +137,8 @@ def renew_book_librarian(request, pk):
 class AuthorCreate(CreateView):
     model = Author
     fields = '__all__'
+
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
