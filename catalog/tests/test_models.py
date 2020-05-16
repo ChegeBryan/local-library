b7539809e7ad2c2554f9832c3e6253e36ecb56bb
Chege Bryan <cbbryan8@gmail.com>
2020-05-16 13:11:57 +0300

    ✅ Test author first name label

--- catalog/tests/test_models.py
+++ catalog/tests/test_models.py
@@ -8,3 +8,8 @@ class AuthorModelTest(TestCase):
     def setUpTestData(cls):
         # Set up no-modified objects used by all test methods
         Author.objects.create(first_name='Big', last_name='Bob')
+
+    def test_first_name_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('first_name').verbose_name
+        self.assertEquals(field_label, 'first name')
