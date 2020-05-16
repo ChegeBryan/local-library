112fe0c4de354a12c383ba1c56f424ab4e462191
Chege Bryan <cbbryan8@gmail.com>
2020-05-16 13:26:13 +0300

    ✅ Test author first name allowed max length

--- catalog/tests/test_models.py
+++ catalog/tests/test_models.py
@@ -18,3 +18,8 @@ class AuthorModelTest(TestCase):
         author = Author.objects.get(id=1)
         field_label = author._meta.get_field('date_of_death').verbose_name
         self.assertEquals(field_label, 'Died')
+
+    def test_first_name_max_length(self):
+        author = Author.objects.get(id=1)
+        max_length = author._meta.get_field('first_name').max_length
+        self.assertEquals(max_length, 100)
